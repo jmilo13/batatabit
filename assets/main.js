d2de758c54f6effc650f423e5e27eaec1b91460e
Camilo <camilogm3@gmail.com>
2021-04-05 22:45:43 -0300

    finaliza y agrega responsive

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,8 @@ const preview = document.getElementById('preview')
 const next = document.getElementById('next')
 const coins = document.getElementById('coins')
 const comissions = document.getElementById('comissions')
+const plans = document.getElementById('plans')
+plans.scrollLeft = (618 - screen.width) / 2
 
 preview.addEventListener('click', change)
 next.addEventListener('click', change)
